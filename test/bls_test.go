079d4f472481f2945176337048d8b343eed5d9bb
Denis Glotov <denis@glotov.org>
2021-09-15 18:02:42 +0200

    Aggregation test

--- test/bls_test.go
+++ test/bls_test.go
@@ -2,11 +2,13 @@ package test
 
 import (
 	"bytes"
+	"log"
+	"math/big"
+	"testing"
+
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
 	"github.com/stretchr/testify/require"
-	"math/big"
-	"testing"
 )
 
 var (
@@ -21,6 +23,23 @@ var (
 
 func TestPrecompiled_VerifySignatureInSolidity(t *testing.T) {
 	_, err := blsSignatureTest.VerifySignature(owner, pubBytes, message, sigBytes)
+	require.NoError(t, err)
+	backend.Commit()
+	verifiedSol, err := blsSignatureTest.Verified(&bind.CallOpts{})
+	require.True(t, verifiedSol)
+}
+
+func TestPrecompiled_VerifyAggregatedSignatureInSolidity(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		secretKey2 := new(big.Int).SetBytes(GenRandomBytes(64))
+		publicKey2 := new(bn256.G2).ScalarBaseMult(secretKey2)
+		signature2 := Sign(secretKey2, message)
+		publicKey = new(bn256.G2).Add(publicKey, publicKey2)
+		signature = new(bn256.G1).Add(signature, signature2)
+	}
+	log.Println("Aggregated public key", publicKey.String())
+	log.Println("Aggregated signature", signature.String())
+	_, err := blsSignatureTest.VerifySignature(owner, publicKey.Marshal(), message, signature.Marshal())
 	require.NoError(t, err)
 	backend.Commit()
 	verifiedSol, err := blsSignatureTest.Verified(&bind.CallOpts{})
